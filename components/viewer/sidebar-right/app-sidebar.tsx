fb9aa62030a8c88175ad3deb74cbc1bcde878ae6
silasjul <silaskierstein@gmail.com>
2026-04-08 11:09:20 +0200

    sidebar resizable right

--- components/viewer/sidebar-right/app-sidebar.tsx
+++ components/viewer/sidebar-right/app-sidebar.tsx
@@ -2,24 +2,41 @@
 
 import * as React from "react";
 
-import {
-  RightSidebar,
-  RightSidebarRail,
-} from "@/components/ui/sidebar-right";
+import { RightSidebar, RightSidebarRail } from "@/components/ui/sidebar-right";
 import {
   SidebarContent,
   SidebarHeader,
   SidebarFooter,
 } from "@/components/ui/sidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export function AppSidebar({
   ...props
 }: React.ComponentProps<typeof RightSidebar>) {
   return (
     <RightSidebar {...props}>
-      <SidebarHeader />
-      <SidebarContent className="gap-0" />
-      <SidebarFooter />
+      <SidebarContent className="gap-0">
+        <ResizablePanelGroup
+          orientation="vertical"
+          className="min-h-[200px] max-w-sm rounded-lg border"
+        >
+          <ResizablePanel defaultSize="25%">
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Header</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize="75%">
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Content</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </SidebarContent>
       <RightSidebarRail />
     </RightSidebar>
   );
