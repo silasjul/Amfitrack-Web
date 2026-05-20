c4df8eb66338a4175338522dd4efb93177c4d03d
silasjul <silaskierstein@gmail.com>
2026-05-20 10:12:52 +0200

    correct start position

--- components/viewer/XRRig.tsx
+++ components/viewer/XRRig.tsx
@@ -3,7 +3,7 @@ import { Group } from "three";
 import { XROrigin } from "@react-three/xr";
 import { useXRFlyLocomotion } from "@/hooks/useXRFlyLocomotion";
 
-const START_POSITION: [number, number, number] = [3, 0, 5];
+const START_POSITION: [number, number, number] = [0, 0, 5];
 
 export default function XRRig() {
   const originRef = useRef<Group>(null);
